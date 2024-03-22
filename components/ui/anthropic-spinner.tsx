ccbcf8438f846d56b569e65fd46f6cdbab607636
Brendan Quinn <brendanquinn89@gmail.com>
2024-03-22 21:31:23 +0000

    Dd

--- components/ui/anthropic-spinner.tsx
+++ components/ui/anthropic-spinner.tsx
@@ -1,20 +1,5 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
-
-const pulsateAnimation = keyframes`
-  0% {
-    transform: scale(1);
-    opacity: 1;
-  }
-  50% {
-    transform: scale(1.2);
-    opacity: 0.6;
-  }
-  100% {
-    transform: scale(1);
-    opacity: 1;
-  }
-`
+import styled from 'styled-components'
 
 const SpinnerContainer = styled.div`
   display: flex;
@@ -22,30 +7,18 @@ const SpinnerContainer = styled.div`
   align-items: center;
   height: 100%;
 `
-
-const SpinnerDot = styled.div`
-  width: 12px;
-  height: 12px;
-  border-radius: 50%;
-  background-color: #000;
-  margin: 0 4px;
-  animation: ${pulsateAnimation} 1.5s infinite ease-in-out;
-
-  &:nth-child(1) {
-    animation-delay: -0.3s;
-  }
-
-  &:nth-child(2) {
-    animation-delay: -0.15s;
-  }
+const SpinnerVideo = styled.video`
+  width: 100px;
+  height: 100px;
 `
 
 export const AnthropicSpinner = () => {
   return (
     <SpinnerContainer>
-      <SpinnerDot />
-      <SpinnerDot />
-      <SpinnerDot />
+      <SpinnerVideo autoPlay loop muted>
+        <source src="components/ui/anthropic-spinner.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </SpinnerVideo>
     </SpinnerContainer>
   )
 }
